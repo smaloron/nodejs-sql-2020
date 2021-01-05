2098c6e828f10bc67a3cc6b8fb1224d7445b1178
Sebastien Maloron <contact@smaloron.eu>
2021-01-05 15:03:49 +0100

    Finalisation de l'API hotel

--- controllers/hotel-controllers.js
+++ controllers/hotel-controllers.js
@@ -24,7 +24,38 @@ const details = (req, res) => {
   );
 };
 
+const deleteOneById = (req, res) => {
+  mysql.query('DELETE FROM hostels WHERE id=?', [req.params.id], (err, data) =>
+    queryCallback(err, data, res)
+  );
+};
+
+const insertOne = (req, res) => {
+  mysql.query(
+    // sql
+    'INSERT INTO hostels SET ?',
+    // données postées
+    req.body,
+    // fonction de callback
+    (err, data) => queryCallback(err, data, res)
+  );
+};
+
+const updateOne = (req, res) => {
+  mysql.query(
+    // sql
+    'UPDATE hostels SET ? WHERE id=?',
+    // données
+    [req.body, req.body.id],
+    // fonction de callback
+    (err, data) => queryCallback(err, data, res)
+  );
+};
+
 module.exports = {
   home,
   details,
+  deleteOneById,
+  insertOne,
+  updateOne,
 };
--- routes/hotel-routes.js
+++ routes/hotel-routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const hotelController = require('../controllers/hotel-controllers');
 
 router.get('/', hotelController.home);
-router.get('/details/:id', hotelController.details);
+router.get('/:id', hotelController.details);
+router.delete('/:id', hotelController.deleteOneById);
+router.post('/', hotelController.insertOne);
+router.put('/', hotelController.updateOne);
 
 module.exports = router;
