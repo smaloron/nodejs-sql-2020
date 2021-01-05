198885c31131e7dbdf16ad894c62dc780b5cb895
Sebastien Maloron <contact@smaloron.eu>
2021-01-05 10:46:47 +0100

    factorisation du callback sur les requêtes SQL

--- controllers/hotel-controllers.js
+++ controllers/hotel-controllers.js
@@ -1,26 +1,26 @@
 const mysql = require('../database/mysql');
 
+const queryCallback = (err, data, res) => {
+  if (err) {
+    res.status(500).json(err);
+  } else if (data.length == 0) {
+    res.status(404).json({ message: 'Aucun résultat pour cette requête' });
+  } else {
+    res.status(200).json(data);
+  }
+};
+
 const home = (req, res) => {
-  mysql.query('SELECT * FROM hostels', (err, data) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(200).json(data);
-    }
-  });
+  mysql.query('SELECT * FROM hostels', (err, data) =>
+    queryCallback(err, data, res)
+  );
 };
 
 const details = (req, res) => {
   mysql.query(
     'SELECT * FROM hostels WHERE id=?',
     [req.params.id],
-    (err, data) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.status(200).json(data);
-      }
-    }
+    (err, data) => queryCallback(err, data, res)
   );
 };
 
