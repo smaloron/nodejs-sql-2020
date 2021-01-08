1adfd7d01d9d4568d7cf670cd74ad848a75c0a25
Sebastien Maloron <contact@smaloron.eu>
2021-01-08 11:11:34 +0100

    Inscription partie frontend

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,25 @@ $(document).ready(function () {
   const $pwdInput = $('#pwd');
   const $loginInput = $('#login');
   const $loginDiv = $('#loginDiv');
+  const $registerDiv = $('#registerDiv');
   const $h1 = $('h1');
+  const $btShowLogin = $('#btShowLogin');
+  const $btShowRegister = $('#btShowRegister');
+  const $registerForm = $('#registerForm');
+
+  // masquer le login et l'inscription
+  $loginDiv.hide();
+  $registerDiv.hide();
+
+  $btShowLogin.click(function () {
+    $loginDiv.toggle();
+    $registerDiv.hide();
+  });
+
+  $btShowRegister.click(function () {
+    $registerDiv.toggle();
+    $loginDiv.hide();
+  });
 
   // Gestion du login
   $loginForm.submit(function (even) {
@@ -26,6 +44,28 @@ $(document).ready(function () {
         user = response.user;
         token = response.token;
         $loginDiv.hide();
+        $h1.text('Bienvenue ' + user.username);
+      })
+      .fail(function (err) {
+        console.log(err);
+      });
+  });
+
+  // Gestion de l'inscription
+  $registerForm.submit(function (even) {
+    even.preventDefault();
+    const data = {
+      login: $("#registerForm input[name='login']").val(),
+      username: $("#registerForm input[name='username']").val(),
+      pwd: $("#registerForm input[name='pwd']").val(),
+    };
+
+    $.post(URL + 'register', data)
+      .done(function (response) {
+        console.log(response);
+        user = response.user;
+        token = response.token;
+        $registerDiv.hide();
         $h1.text('Bienvenue ' + user.username);
       })
       .fail(function (err) {
